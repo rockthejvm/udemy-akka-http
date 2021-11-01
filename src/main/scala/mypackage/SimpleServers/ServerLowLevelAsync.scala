d9215b7da9cf859fe1167f83c227aa63246cc0f0
Deepak Jayaprakash <djayapr1@in.ibm.com>
2021-11-01 20:29:24 +0530

    working code

--- src/main/scala/mypackage/SimpleServers/ServerLowLevelAsync.scala
+++ src/main/scala/mypackage/SimpleServers/ServerLowLevelAsync.scala
@@ -4,9 +4,12 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.model.headers.Location
+import akka.http.scaladsl.server.Directives.{complete, failWith, onComplete}
 import akka.stream.ActorMaterializer
-
+import org.json.JSONObject
+import scala.concurrent.duration._
 import scala.concurrent.Future
+import scala.util.{Failure, Success}
 
 object ServerLowLevelAsync extends  App {
 
@@ -19,11 +22,28 @@ object ServerLowLevelAsync extends  App {
   val response = HttpEntity(ContentTypes.`text/html(UTF-8)`,html)
   val response2 = HttpEntity(ContentTypes.`text/html(UTF-8)`,htmlerror)
 
+  val jsonObject: JSONObject = new JSONObject();
+  jsonObject.put("name", "deepak");
+  jsonObject.put("address", "Bangalore");
+  val jsonresponse = HttpEntity(ContentTypes.`application/json`, jsonObject.toString)
+
   // method, URI, HTTP headers, content and the protocol (HTTP1.1/HTTP2.0) -- decompose
   val asyncRequestHandler: HttpRequest => Future[HttpResponse] = {
                                 case HttpRequest(HttpMethods.GET, Uri.Path("/home"), _, _, _) => Future(HttpResponse(entity = response))
                                 case HttpRequest(HttpMethods.GET, Uri.Path("/search"), _, _, _) => Future(HttpResponse(StatusCodes.Found,
                                                                                                                         headers = List(Location("http://google.com"))))
+                                case HttpRequest(HttpMethods.POST, Uri.Path("/json"), _, entity, _) =>
+                                  val strictEntityFuture = entity.toStrict(2 seconds) //convert to future with wait of 2 sec
+                                  val jsonFuture = strictEntityFuture.map(_.data.utf8String) //Byte stream to String
+                                  onComplete(jsonFuture){
+                                    case Success(jsondata) =>
+                                                            val j:JSONObject=new JSONObject(jsondata)
+                                                              println(j.get("name").toString)
+                                                              complete(StatusCodes.OK)
+                                    case Failure(ex) =>   complete(StatusCodes.InternalServerError)
+                                  }
+                                    Future(HttpResponse(StatusCodes.Found)  )
+
                                 case request: HttpRequest => {request.discardEntityBytes()
                                                               Future(HttpResponse(StatusCodes.NotFound, entity = response2)) //StatusCodes.OK,
                                                               }
